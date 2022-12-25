23047d8cd24560bb77f2757fb85934aa6bce15b2
user <user@localhost>
2022-12-25 14:26:41 +0000

    Use shlex.split for parsing download_command from config;
    Add new arguments to YT2DL: url_placeholder and on_success callback

--- dl_module.py
+++ dl_module.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import os
+import shlex
 
 
 URL_PLACEHOLDER = '{url}'
@@ -8,11 +9,21 @@ URL_PLACEHOLDER = '{url}'
 
 class YT2DL():
 
-    def __init__(self, download_command):
+    def __init__(self, download_command, url_placeholder=URL_PLACEHOLDER):
         self.downloads = {}
+        self.url_placeholder = url_placeholder
         self.command = download_command
+        try:
+            self.args = shlex.split(self.command)
+        except ValueError as e:
+            logging.error(f'Error parsing "download_command" string {self.command}: {e}')
+            raise
 
-    def add(self, url, save_path, on_failure=lambda: None):
+    def args_for(self, url):
+        # need a copy of template arguments list anyway, since it gets changed
+        return [url if arg == self.url_placeholder else arg for arg in self.args]
+
+    def add(self, url, save_path, on_failure=lambda: None, on_success=lambda: None):
         if save_path is not None:
             if not os.path.exists(save_path):
                 logging.warning('download directory {} does not exist, creating'.format(save_path))
@@ -22,12 +33,14 @@ class YT2DL():
         else:
             logging.debug('downloader for {} was called already'.format(url))
 
-    async def start_downloader(self, url, save_path=None, on_failure=lambda: None):
-        args = self.command.replace(URL_PLACEHOLDER, url).split()
+    async def start_downloader(self, url, save_path=None, on_failure=lambda: None, on_success=lambda: None):
+        args = self.args_for(url)
         logging.info('starting download subprocess for {}'.format(url))
         process = await asyncio.create_subprocess_exec(*args, cwd=save_path)
         await process.wait()
         logging.debug('download subprocess for {} finished with exit code {}'.format(url, process.returncode))
         self.downloads.pop(url)
-        if process.returncode != 0:
+        if process.returncode == 0:
+            on_success()
+        else:
             on_failure()
